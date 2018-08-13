c693d0267366bb80997d66e26acb324819108bd2
Mathieu Léonardon <mathieu.leonardon@u-bordeaux.fr>
2018-08-13 19:28:26 +0200

    Huge curves update.

--- main/ch1_fig/curves/comment_uncomment_legend_line.sh
+++ main/ch1_fig/curves/comment_uncomment_legend_line.sh
@@ -6,9 +6,11 @@ if [ -z "$1" ]
 elif [ $1 = "on" ]
   then
   	find . -type f -name "*.txt" -exec sed -i 's/      Es/#     Es/g' {} +
+  	find . -type f -name "*.txt" -exec sed -i 's/\r//g' {} +
 elif [ $1 = "off" ]
   then
   	find . -type f -name "*.txt" -exec sed -i 's/#     Es/      Es/g' {} +
+  	find . -type f -name "*.txt" -exec sed -i 's/\r//g' {} +
 else
     echo "Wrong arg"
 fi
